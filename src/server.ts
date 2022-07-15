53ce06c1b00a2c694de08657759408b8017953e0
Juliano Boese <juliano.boese@gmail.com>
2022-07-15 13:25:38 -0300

    chore: :tada: install and configure eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import app from "./app"
+import app from './app';
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`));

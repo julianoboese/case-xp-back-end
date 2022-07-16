9118e78c8b91ee623c3114f59338c6f057e0a3a2
Juliano Boese <juliano.boese@gmail.com>
2022-07-15 21:48:48 -0300

    refactor(order): :necktie: refactor order service not to allow short selling

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,6 +1,7 @@
 import { UserAsset } from '@prisma/client';
 import IOrder from '../interfaces/order.interface';
 import prisma from '../prisma';
+import HttpError from '../utils/http.error';
 
 export default class OrderService {
   public static buyAsset = async (order: IOrder): Promise<UserAsset> => {
@@ -18,10 +19,17 @@ export default class OrderService {
   public static sellAsset = async (order: IOrder): Promise<UserAsset> => {
     const { userId, assetId, amount } = order;
 
-    const position = await prisma.userAsset.upsert({
+    const userAsset = await prisma.userAsset.findUnique({
       where: { userId_assetId: { userId, assetId } },
-      update: { quantity: { decrement: amount } },
-      create: { userId, assetId, quantity: -amount },
+    });
+
+    if (!userAsset) throw new HttpError(404, 'Ativo não consta na carteira.');
+
+    if (userAsset.quantity < amount) throw new HttpError(400, 'Quantidade insuficiente na carteira.');
+
+    const position = await prisma.userAsset.update({
+      where: { userId_assetId: { userId, assetId } },
+      data: { quantity: { decrement: amount } },
     });
 
     return position;

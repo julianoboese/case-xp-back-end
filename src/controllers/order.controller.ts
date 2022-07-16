d65f7266da425057cc45f1f3a6999e460aeca07c
Juliano Boese <juliano.boese@gmail.com>
2022-07-16 00:30:27 -0300

    refactor(order): :lock: refactor order controller to get user id from token

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,13 +3,13 @@ import OrderService from '../services/order.service';
 
 export default class OrderController {
   public static buyAsset = async (req: Request, res: Response): Promise<void> => {
-    const userOrder = await OrderService.buyAsset(req.body);
+    const userOrder = await OrderService.buyAsset({ userId: res.locals.user.id, ...req.body });
 
     res.status(200).json(userOrder);
   };
 
   public static sellAsset = async (req: Request, res: Response): Promise<void> => {
-    const userAsset = await OrderService.sellAsset(req.body);
+    const userAsset = await OrderService.sellAsset({ userId: res.locals.user.id, ...req.body });
 
     res.status(200).json(userAsset);
   };

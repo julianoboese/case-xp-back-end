796e93593f1f20a838ff046b141b8119c5cb890a
Juliano Boese <juliano.boese@gmail.com>
2022-07-15 21:30:25 -0300

    feat(register): :sparkles: create register interface and service class

---
+++ src/services/register.service.ts
@@ -0,0 +1,24 @@
+import IRegister from '../interfaces/register.interface';
+import prisma from '../prisma';
+import HttpError from '../utils/http.error';
+import Jwt from '../utils/jwt';
+
+export default class RegisterService {
+  public static register = async (newUser: IRegister): Promise<string> => {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: newUser.email,
+      },
+    });
+
+    if (user) throw new HttpError(409, 'Usuário já possui conta.');
+
+    const createdUser = await prisma.user.create({
+      data: newUser,
+    });
+
+    const token = Jwt.generateToken(createdUser);
+
+    return token;
+  };
+}

a80562d8dfa1b5d7338e135603194b250433e3ae
Juliano Boese <juliano.boese@gmail.com>
2022-07-16 10:33:30 -0300

    chore: :heavy_plus_sign: install and apply cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import 'express-async-errors';
 import ErrorMiddleware from './middlewares/error.middleware';
@@ -7,6 +8,8 @@ import routes from './routes';
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 

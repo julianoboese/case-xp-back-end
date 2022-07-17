c0f680c7be5d9c0f12ec276594730fd229d85bdf
Juliano Boese <juliano.boese@gmail.com>
2022-07-17 15:55:56 -0300

    test(asset): :white_check_mark: create unit tests for asset service

--- src/services/asset.service.ts
+++ src/services/asset.service.ts
@@ -1,17 +1,17 @@
-import fetch from 'cross-fetch';
+// import fetch from 'cross-fetch';
 import IUserAsset from '../interfaces/asset.interface';
 import prisma from '../prisma';
 import HttpError from '../utils/http.error';
 
 export default class AssetService {
-  private static fetchAssetPrice = async (ticker: string) => {
-    const { B3_API_KEY } = process.env;
-    const b3ApiUrl = `https://api.hgbrasil.com/finance/stock_price?key=${B3_API_KEY}&symbol=${ticker}`;
-    const assetData = await (await fetch(b3ApiUrl)).json();
+  // private static fetchAssetPrice = async (ticker: string) => {
+  //   const { B3_API_KEY } = process.env;
+  //   const b3ApiUrl = `https://api.hgbrasil.com/finance/stock_price?key=${B3_API_KEY}&symbol=${ticker}`;
+  //   const assetData = await (await fetch(b3ApiUrl)).json();
 
-    const { price, change_percent: change } = assetData.results[ticker];
-    return { price, change };
-  };
+  //   const { price, change_percent: change } = assetData.results[ticker];
+  //   return { price, change };
+  // };
 
   private static mockFetchAssetPrice = async () => ({ price: 25.6, change: -5.65 });
 
---
+++ tests/asset.service.test.ts
@@ -0,0 +1,94 @@
+import { prismaMock } from "./prisma.mock";
+import AssetService from '../src/services/asset.service';
+
+describe('The AssetService getAssets function', () => {
+
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return the user assets', async () => {
+    const assetsMock = [{
+      userId: 1,
+      assetId: 1,
+      quantity: 100,
+      asset: {
+        ticker: 'PRIO3',
+      },
+    },
+    {
+      userId: 1,
+      assetId: 2,
+      quantity: 200,
+      asset: {
+        ticker: 'AMBP3',
+      },
+    }]
+
+    prismaMock.userAsset.findMany.mockResolvedValue(assetsMock);
+
+    jest.spyOn(AssetService, <any>'mockFetchAssetPrice')
+      .mockResolvedValueOnce({price: 20.57, change: -1.5})
+      .mockResolvedValue({price: 5.6, change: 0.23})
+
+    const expectedReturn = [{
+      userId: 1,
+      assetId: 1,
+      ticker: 'PRIO3',
+      quantity: 100,
+      price: 20.57,
+      change: -1.5,
+    },
+    {
+      userId: 1,
+      assetId: 2,
+      ticker: 'AMBP3',
+      quantity: 200,
+      price: 5.6,
+      change: 0.23,
+    }]
+
+    const assets = await AssetService.getAssets(1);
+  
+    expect(assets).toEqual(expectedReturn);
+  });
+});
+
+describe('The AssetService getAsset function', () => {
+
+  afterEach(() => jest.clearAllMocks());
+
+  it('should throw an error if the user doesn\'t own it', async () => {
+    prismaMock.userAsset.findUnique.mockResolvedValue(null);
+
+    await expect(AssetService.getAsset(1, 3))
+        .rejects.toThrow('Ativo não consta na carteira.');
+  });
+
+  it('should return the asset if the user owns it', async () => {
+    const assetMock = {
+      userId: 1,
+      assetId: 1,
+      quantity: 100,
+      asset: {
+        ticker: 'PRIO3',
+      }
+    }
+
+    prismaMock.userAsset.findUnique.mockResolvedValue(assetMock);
+
+    jest.spyOn(AssetService, <any>'mockFetchAssetPrice')
+      .mockResolvedValue({price: 20.57, change: -1.5})
+
+    const expectedReturn = {
+      userId: 1,
+      assetId: 1,
+      ticker: 'PRIO3',
+      quantity: 100,
+      price: 20.57,
+      change: -1.5,
+    }
+
+    const assets = await AssetService.getAsset(1, 1);
+  
+    expect(assets).toEqual(expectedReturn);
+  });
+});

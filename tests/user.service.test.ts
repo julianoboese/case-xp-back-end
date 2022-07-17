c5ef0ec50596e6c525e1c28c71787d6de8ab0826
Juliano Boese <juliano.boese@gmail.com>
2022-07-17 16:15:11 -0300

    test(user): :white_check_mark: create unit tests for user service

---
+++ tests/user.service.test.ts
@@ -0,0 +1,21 @@
+import { prismaMock } from "./prisma.mock";
+import UserService from '../src/services/user.service';
+import { User } from "@prisma/client";
+
+describe('The UserService getUser function', () => {
+
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return the user name', async () => {
+    const userMock = {
+      firstName: 'Jon',
+      lastName: 'Doe',
+    };
+
+    prismaMock.user.findUnique.mockResolvedValue(userMock as User);
+
+    const user = await UserService.getUser(1);
+
+    expect(user).toEqual(userMock);
+  });
+});

3dde350d64f6b3384b86935d743d7ba2a3747529
Juliano Boese <juliano.boese@gmail.com>
2022-07-17 18:39:17 -0300

    test(login): :white_check_mark: create unit tests for login controller

---
+++ tests/controllers/login.controller.test.ts
@@ -0,0 +1,35 @@
+import LoginService from '../../src/services/login.service';
+import LoginController from '../../src/controllers/login.controller';
+import { Request, Response } from 'express';
+
+describe('The LoginController login function', () => {
+  
+  const requestMock = { body: { email: 'jon.doe@email.com', password: '12345678' }};
+  const responseMock = {
+    status: jest.fn(),
+    json: jest.fn(),
+  }
+  
+  beforeEach(() => {
+    jest.spyOn(LoginService, 'login').mockResolvedValue('q1w2e3r4t5');
+    responseMock.status.mockReturnValue(responseMock);
+  })
+
+  afterEach(() => jest.clearAllMocks());
+
+  it('should respond with status code 200', async () => {
+    await LoginController.login(requestMock as Request, responseMock as unknown as Response)
+
+    expect(responseMock.status).toHaveBeenCalled();
+    expect(responseMock.status).toHaveBeenCalledTimes(1);
+    expect(responseMock.status).toHaveBeenCalledWith(200);
+  });
+
+  it('should respond with a token', async () => {
+    await LoginController.login(requestMock as Request, responseMock as unknown as Response)
+
+    expect(responseMock.json).toHaveBeenCalled();
+    expect(responseMock.json).toHaveBeenCalledTimes(1);
+    expect(responseMock.json).toHaveBeenCalledWith(expect.objectContaining({ token: 'q1w2e3r4t5' }));
+  });
+});

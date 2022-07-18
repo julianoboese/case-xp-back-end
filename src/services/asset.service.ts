8aa8245506ab7cb076492f7ef9c951deb9f3d19c
Juliano Boese <juliano.boese@gmail.com>
2022-07-18 09:50:44 -0300

    feat(asset): :sparkles: create service function to get all assets in broker

--- src/services/asset.service.ts
+++ src/services/asset.service.ts
@@ -1,3 +1,4 @@
+import { Asset } from '@prisma/client';
 import IUserAsset from '../interfaces/asset.interface';
 import prisma from '../prisma';
 import AssetInfo from '../utils/asset.info';
@@ -7,16 +8,23 @@ export default class AssetService {
   public static getAssets = async (userId: number): Promise<IUserAsset[]> => {
     const userAssets = await prisma.userAsset.findMany({
       where: { userId },
-      select: { userId: true, assetId: true, quantity: true, asset: { select: { ticker: true } } },
+      select: {
+        userId: true,
+        assetId: true,
+        quantity: true,
+        asset: { select: { ticker: true } },
+      },
     });
 
-    const userAssetsWithInfo = await Promise.all(userAssets.map(async (userAsset) => {
-      const { assetId, quantity, asset } = userAsset;
-      const { ticker } = asset;
-      const { price, change } = await AssetInfo.mockFetchAssetPrice();
+    const userAssetsWithInfo = await Promise.all(
+      userAssets.map(async (userAsset) => {
+        const { assetId, quantity, asset } = userAsset;
+        const { ticker } = asset;
+        const { price, change } = await AssetInfo.mockFetchAssetPrice();
 
-      return { userId, assetId, ticker, quantity, price, change };
-    }));
+        return { userId, assetId, ticker, quantity, price, change };
+      }),
+    );
 
     return userAssetsWithInfo;
   };
@@ -24,13 +32,20 @@ export default class AssetService {
   public static getAsset = async (userId: number, assetId: number): Promise<IUserAsset | null> => {
     const userAsset = await prisma.userAsset.findUnique({
       where: { userId_assetId: { userId, assetId } },
+      select: {
+        userId: true,
+        assetId: true,
+        quantity: true,
+        asset: { select: { ticker: true } },
+      },
+    });
 
     // Caso a pessoa usuária não possua o ativo, ele é buscado na base da corretora
     if (!userAsset) {
       const asset = await prisma.asset.findUnique({
         where: { id: assetId },
         select: { id: true, ticker: true },
-    });
+      });
 
       if (!asset) throw new HttpError(404, 'Ativo indisponível na corretora.');
 
@@ -46,5 +61,13 @@ export default class AssetService {
     const { price, change } = await AssetInfo.mockFetchAssetPrice();
 
     return { userId, assetId, ticker, quantity, price, change };
+  };
+
+  public static getAllAssets = async (): Promise<Omit<Asset, 'quantity'>[]> => {
+    const assets = await prisma.asset.findMany({
+      select: { id: true, ticker: true, name: true },
+    });
+
+    return assets;
   };
 }

77f1cbaf603937cb25de8f9411e3c9d52e663bc2
Juliano Boese <juliano.boese@gmail.com>
2022-07-18 17:29:23 -0300

    fix(user): :fire: remove debug code

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,12 +3,8 @@ import UserService from '../services/user.service';
 
 export default class UserController {
   public static getUser = async (_req: Request, res: Response): Promise<void> => {
-    try {
-      const user = await UserService.getUser(res.locals.user.id);
+    const user = await UserService.getUser(res.locals.user.id);
 
-      res.status(200).json(user);
-    } catch (error) {
-      console.log(error);
-    }
+    res.status(200).json(user);
   };
 }

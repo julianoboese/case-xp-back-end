5fabb97d9e9da4f54be2194fac84459c964f56d9
Juliano Boese <juliano.boese@gmail.com>
2022-07-19 16:39:05 -0300

    test(valid): :white_check_mark: create unit tests for validation middleware

---
+++ tests/unit/middlewares/validation.middleware.test.ts
@@ -0,0 +1,29 @@
+import ValidationMiddleware from '../../../src/middlewares/validation.middleware';
+import { NextFunction, Request, Response } from 'express';
+import loginValidator from '../../../src/validators/login.validator';
+
+describe('The ValidationMiddleware "validate" function', () => {
+  
+  const sampleValidation = new ValidationMiddleware(loginValidator);
+
+  const responseMock = {};
+  const nextMock = jest.fn();
+
+  it('should throw an error if request is invalid', async () => {
+    const requestMock = { body: { email: 'felipe.silva@email.com' } };
+
+    expect(() => {
+      sampleValidation.validate(requestMock as Request, responseMock as unknown as Response, nextMock as NextFunction)
+    }).toThrow('"password" is required');
+  });
+
+  it('should call "next" function if request is valid', async () => {
+  const requestMock = { body: { email: 'felipe.silva@email.com', password: '12345678' } };
+
+    sampleValidation.validate(requestMock as Request, responseMock as unknown as Response, nextMock as NextFunction)
+
+    expect(nextMock).toHaveBeenCalled();
+    expect(nextMock).toHaveBeenCalledTimes(1);
+    expect(nextMock).toHaveBeenCalledWith();
+  });
+});

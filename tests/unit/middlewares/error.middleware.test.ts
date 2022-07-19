f2348d1572c2d243ceed3611bc39a640102622a9
Juliano Boese <juliano.boese@gmail.com>
2022-07-19 16:20:05 -0300

    test(error): :white_check_mark: create unit tests for error middleware

---
+++ tests/unit/middlewares/error.middleware.test.ts
@@ -0,0 +1,59 @@
+import ErrorMiddleware from '../../../src/middlewares/error.middleware';
+import { NextFunction, Request, Response } from 'express';
+import HttpError from '../../../src/utils/http.error';
+
+describe('The AuthMiddleware "auth" function', () => {
+  
+  const requestMock = {};
+  const responseMock = {
+    status: jest.fn(),
+    json: jest.fn(),
+  }
+  const nextMock = jest.fn();
+
+  beforeEach(() => {
+    responseMock.status.mockReturnValue(responseMock);
+  })
+
+  afterEach(() => jest.clearAllMocks());
+
+  it('should respond with status code 401', async () => {
+    const errorMock = { status: 401, message: 'Acesso não autorizado.' }
+
+    ErrorMiddleware.handleError(errorMock as HttpError, requestMock as Request, responseMock as unknown as Response, nextMock as NextFunction)
+
+    expect(responseMock.status).toHaveBeenCalled();
+    expect(responseMock.status).toHaveBeenCalledTimes(1);
+    expect(responseMock.status).toHaveBeenCalledWith(401);
+  });
+
+  it('should respond with a custom error message', async () => {
+    const errorMock = { status: 401, message: 'Acesso não autorizado.' }
+
+    ErrorMiddleware.handleError(errorMock as HttpError, requestMock as Request, responseMock as unknown as Response, nextMock as NextFunction)
+
+    expect(responseMock.json).toHaveBeenCalled();
+    expect(responseMock.json).toHaveBeenCalledTimes(1);
+    expect(responseMock.json).toHaveBeenCalledWith({ message: 'Acesso não autorizado.' });
+  });
+
+  it('should respond with status code 500', async () => {
+    const errorMock = { message: 'Internal Server Error' }
+
+    ErrorMiddleware.handleError(errorMock as HttpError, requestMock as Request, responseMock as unknown as Response, nextMock as NextFunction)
+
+    expect(responseMock.status).toHaveBeenCalled();
+    expect(responseMock.status).toHaveBeenCalledTimes(1);
+    expect(responseMock.status).toHaveBeenCalledWith(500);
+  });
+
+  it('should respond with a standard error message', async () => {
+    const errorMock = { message: 'Internal Server Error' }
+
+    ErrorMiddleware.handleError(errorMock as HttpError, requestMock as Request, responseMock as unknown as Response, nextMock as NextFunction)
+
+    expect(responseMock.json).toHaveBeenCalled();
+    expect(responseMock.json).toHaveBeenCalledTimes(1);
+    expect(responseMock.json).toHaveBeenCalledWith({ message: 'Internal Server Error' });
+  });
+});

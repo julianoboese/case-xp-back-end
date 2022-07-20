d33e258a33f2ef626e390625a4a3504a23af16cc
Juliano Boese <juliano.boese@gmail.com>
2022-07-20 20:50:55 -0300

    fix: :rotating_light: fix linter warnings in utils unit tests

--- tests/unit/utils/jwt.test.ts
+++ tests/unit/utils/jwt.test.ts
@@ -2,39 +2,43 @@ import jwt from 'jsonwebtoken';
 import Jwt from '../../../src/utils/jwt';
 
 describe('The Jwt "generateToken" function', () => {
-
   afterEach(() => jest.clearAllMocks());
 
   it('should return the token signed', async () => {
     jest.spyOn(jwt, 'sign').mockImplementation(() => 'q1w2e3r4t5');
 
-    const token = Jwt.generateToken({id: 1, email: 'felipe.silva@email.com'})
-    
+    const token = Jwt.generateToken({ id: 1, email: 'felipe.silva@email.com' });
+
     expect(token).toBe('q1w2e3r4t5');
   });
 });
 
 describe('The Jwt "authenticateToken" function', () => {
-
   afterEach(() => jest.clearAllMocks());
 
   it('should throw an error if token is not provided', async () => {
-    await expect(Jwt.authenticateToken())
-      .rejects.toThrow('Acesso não autorizado.')
+    await expect(Jwt.authenticateToken()).rejects.toThrow(
+      'Acesso não autorizado.',
+    );
   });
 
   it('should return user data if token is verified', async () => {
-    jest.spyOn(jwt, 'verify').mockImplementation(() => { throw new Error() });
+    jest.spyOn(jwt, 'verify').mockImplementation(() => {
+      throw new Error();
+    });
 
-    await expect(Jwt.authenticateToken('t5r4e3w2q1'))
-    .rejects.toThrow('Acesso não autorizado.')
+    await expect(Jwt.authenticateToken('t5r4e3w2q1')).rejects.toThrow(
+      'Acesso não autorizado.',
+    );
   });
 
   it('should return user data if token is verified', async () => {
-    jest.spyOn(jwt, 'verify').mockImplementation(() => ({id: 1, email: 'felipe.silva@email.com'}));
+    jest
+      .spyOn(jwt, 'verify')
+      .mockImplementation(() => ({ id: 1, email: 'felipe.silva@email.com' }));
 
-    const user = await Jwt.authenticateToken('q1w2e3r4t5')
+    const user = await Jwt.authenticateToken('q1w2e3r4t5');
 
-    expect(user).toEqual({ id: 1, email: 'felipe.silva@email.com' })
+    expect(user).toEqual({ id: 1, email: 'felipe.silva@email.com' });
   });
 });

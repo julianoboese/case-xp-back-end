98697534fc752112a4a3beb4cb540c4396c2b1e6
Juliano Boese <juliano.boese@gmail.com>
2022-07-20 20:50:27 -0300

    fix: :rotating_light: fix linter warnings in middlewares unit tests

--- tests/unit/middlewares/validation.middleware.test.ts
+++ tests/unit/middlewares/validation.middleware.test.ts
@@ -1,9 +1,8 @@
-import ValidationMiddleware from '../../../src/middlewares/validation.middleware';
 import { NextFunction, Request, Response } from 'express';
+import ValidationMiddleware from '../../../src/middlewares/validation.middleware';
 import loginValidator from '../../../src/validators/login.validator';
 
 describe('The ValidationMiddleware "validate" function', () => {
-  
   const sampleValidation = new ValidationMiddleware(loginValidator);
 
   const responseMock = {};
@@ -13,14 +12,24 @@ describe('The ValidationMiddleware "validate" function', () => {
     const requestMock = { body: { email: 'felipe.silva@email.com' } };
 
     expect(() => {
-      sampleValidation.validate(requestMock as Request, responseMock as unknown as Response, nextMock as NextFunction)
+      sampleValidation.validate(
+        requestMock as Request,
+        responseMock as unknown as Response,
+        nextMock as NextFunction,
+      );
     }).toThrow('"password" is required');
   });
 
   it('should call "next" function if request is valid', async () => {
-  const requestMock = { body: { email: 'felipe.silva@email.com', password: '12345678' } };
+    const requestMock = {
+      body: { email: 'felipe.silva@email.com', password: '12345678' },
+    };
 
-    sampleValidation.validate(requestMock as Request, responseMock as unknown as Response, nextMock as NextFunction)
+    sampleValidation.validate(
+      requestMock as Request,
+      responseMock as unknown as Response,
+      nextMock as NextFunction,
+    );
 
     expect(nextMock).toHaveBeenCalled();
     expect(nextMock).toHaveBeenCalledTimes(1);

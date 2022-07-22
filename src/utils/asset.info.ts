354d8d9a25eef7378e6d6328efe87400b0d431ae
Juliano Boese <juliano.boese@gmail.com>
2022-07-21 21:37:35 -0300

    chore(asset): :bulb: add comment

--- src/utils/asset.info.ts
+++ src/utils/asset.info.ts
@@ -7,6 +7,7 @@ export default class AssetInfo {
     const b3ApiUrl = `https://api.hgbrasil.com/finance/stock_price?key=${B3_API_KEY}&symbol=${ticker}`;
     const assetData = await (await fetch(b3ApiUrl)).json();
 
+    // Em ambientes que não sejam de produção, o preço e a variação dos ativos são "mockados"
     if (assetData.results.error) {
       const randomPrice = +(Math.random() * 60).toFixed(2);
       const randomChange = +(Math.random() * 10 - 5).toFixed(2);

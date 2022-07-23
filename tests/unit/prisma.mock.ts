ad636d10876c44871c02c232d8d5ada4ff8d9a0a
Juliano Boese <juliano.boese@gmail.com>
2022-07-23 18:30:45 -0300

    refactor: :truck: move prisma mock inside unit tests folder

--- tests/unit/prisma.mock.ts
+++ tests/unit/prisma.mock.ts
@@ -2,7 +2,7 @@
 // https://www.prisma.io/docs/guides/testing/unit-testing
 import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
-import prisma from '../src/prisma';
+import prisma from '../../src/prisma';
 
 jest.mock('../src/prisma', () => ({
   __esModule: true,
